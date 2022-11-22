8ac4fc22abedc43396b76784d61dc2c216f8e21c
isma-91 <isma91@hotmail.it>
2022-11-22 14:46:34 +0100

    Add mail list with styles

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/*
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+new Vue({
+  el: "#root",
+  data: {
+    arrMails: [],
+  },
+  methods: {},
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((result) => {
+          // console.log(result.data.response);
+          this.arrMails.push(result.data.response);
+          console.log(this.arrMails);
+        });
+    }
+  },
+});
